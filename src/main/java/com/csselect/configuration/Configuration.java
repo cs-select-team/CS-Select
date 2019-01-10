6eb862fa30c1fffc2d7a08993e632b5ef1a04642
Alexander Linder <alexander-linder@gmx.de>
2019-01-11 00:13:35 +0100

    Complete Configuration-Interface

--- src/main/java/com/csselect/configuration/Configuration.java
+++ src/main/java/com/csselect/configuration/Configuration.java
@@ -5,4 +5,52 @@ package com.csselect.configuration;
  * Allows access to configuration values and is realized as singleton
  */
 public interface Configuration {
+
+    /**
+     * Gets the password organisers have to use to be able to register
+     * @return organiser password
+     */
+    String getOrganiserPassword();
+
+    /**
+     * Gets the URL of the Machine-Learning-Server used
+     * @return ML-Server URL
+     */
+    String getMLServerURL();
+
+    /**
+     * Gets the type of database used
+     * @return database type
+     */
+    String getDatabaseType();
+
+    /**
+     * Gets the hostname under which the database-server is located
+     * @return hostname of the database-server
+     */
+    String getDatabaseHostname();
+
+    /**
+     * Gets the port of the database-server
+     * @return port of the database-server
+     */
+    int getDatabaseport();
+
+    /**
+     * Gets the username of the database user to be used
+     * @return username
+     */
+    String getDatabaseUsername();
+
+    /**
+     * Gets the password of the database user to be used
+     * @return password
+     */
+    String getDatabasePassword();
+
+    /**
+     * Gets the path to the home directory
+     * @return home directory path
+     */
+    String getHomeDirectory();
 }
