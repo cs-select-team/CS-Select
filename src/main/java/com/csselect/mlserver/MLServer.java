49f54ce8653bbb1aabfc8e79d1408adfc8b3b6f4
Alexander Linder <alexander-linder@gmx.de>
2019-01-10 17:25:35 +0100

    Add interfaces for mlserver package

---
+++ src/main/java/com/csselect/mlserver/MLServer.java
@@ -0,0 +1,9 @@
+package com.csselect.mlserver;
+
+/**
+ * Abstraction of the Machine-Learning-Server being used to evaluate feature selections
+ * Realized as singleton to ensure only one instance can exist at all time
+ */
+public interface MLServer {
+
+}
