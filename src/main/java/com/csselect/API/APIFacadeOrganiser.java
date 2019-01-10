57ebaf94dcb93a5b7546deb1de2752d98779e575
Bendix Sonnenberg <bendix.sonnenberg@gmx.de>
2019-01-10 19:47:47 +0100

    Add Servlet dependency in pom and Classes from API

---
+++ src/main/java/com/csselect/API/APIFacadeOrganiser.java
@@ -0,0 +1,7 @@
+package com.csselect.API;
+
+public class APIFacadeOrganiser extends APIFacadeUser {
+    public void register(String email, String password, String param3) {
+
+    }
+}
---
+++ src/main/java/com/csselect/API/httpAPI/Games.java
@@ -0,0 +1,16 @@
+package com.csselect.API.httpAPI;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Games extends HttpServlet {
+    @Override
+    public void doGet(HttpServletRequest req, HttpServletResponse resp) {
+
+    }
+
+    @Override
+    public void doPost(HttpServletRequest req, HttpServletResponse resp) {
+
+    }
+}
