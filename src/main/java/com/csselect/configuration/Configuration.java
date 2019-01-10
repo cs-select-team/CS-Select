c29db3a9d4d48755d908a6dd5d79777dfad337d8
Alexander Linder <alexander-linder@gmx.de>
2019-01-10 17:25:20 +0100

    Add interfaces for configuration package

---
+++ src/main/java/com/csselect/configuration/Configuration.java
@@ -0,0 +1,8 @@
+package com.csselect.configuration;
+
+/**
+ * Abstraction of the configuration implementation used
+ * Allows access to configuration values and is realized as singleton
+ */
+public interface Configuration {
+}
