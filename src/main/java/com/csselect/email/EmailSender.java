ed308a6bfaa696d48fa8deeef5a6ffa4e16b7b97
Alexander Linder <alexander-linder@gmx.de>
2019-01-10 17:26:40 +0100

    Add barebones EmailSender class to email package

---
+++ src/main/java/com/csselect/email/EmailSender.java
@@ -0,0 +1,21 @@
+package com.csselect.email;
+
+/**
+ * Class handling the sending of emails
+ */
+public final class EmailSender {
+
+    private EmailSender() {
+        //No instances of this class should be instantiated
+    }
+
+    /**
+     * Sends an email with the given heading and message to the recipient
+     * @param recipient email address to send the mail to
+     * @param header the emails header
+     * @param message the emails message
+     */
+    public static void sendEmail(String recipient, String header, String message) {
+
+    }
+}
