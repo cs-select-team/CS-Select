6275cb4c3875eef149aa60e5f723b16c072eeb6f
Bendix Sonnenberg <bendix.sonnenberg@gmx.de>
2019-01-11 15:38:28 +0100

    add methods from design to classes

--- src/main/java/com/csselect/API/APIFacadeOrganiser.java
+++ src/main/java/com/csselect/API/APIFacadeOrganiser.java
@@ -1,7 +1,56 @@
 package com.csselect.API;
 
+import com.csselect.game.Game;
+import com.csselect.game.gamecreation.patterns.Pattern;
+import com.csselect.user.Organiser;
+
+import java.util.LinkedList;
+import java.util.List;
+
 public class APIFacadeOrganiser extends APIFacadeUser {
+    private Organiser organiser;
     public void register(String email, String password, String param3) {
 
     }
+
+    public Organiser getOrganiser() {
+        return organiser;
+    }
+
+    public List<Game> getActiveGames() {
+        return new LinkedList<>();
+    }
+
+    public List<Game> getTerminatedGames() {
+        return new LinkedList<>();
+    }
+
+    public List<Pattern> setGameOption() {
+        return new LinkedList<>();
+    }
+
+    public void savePattern(String title) {
+
+    }
+
+    public void loadPattern(Pattern pattern) {
+
+    }
+
+    public boolean createGame() {
+        return false;
+    }
+
+    public void invitePlayer(String playerEmail, int gameId) {
+
+    }
+
+    public void terminateGame(int gameId) {
+
+    }
+
+    public void deleteGame(int gameId) {
+
+    }
+
 }
--- src/main/java/com/csselect/API/APIFacadePlayer.java
+++ src/main/java/com/csselect/API/APIFacadePlayer.java
@@ -1,7 +1,84 @@
 package com.csselect.API;
 
+import com.csselect.game.Feature;
+import com.csselect.game.Game;
+import com.csselect.gamification.Achievement;
+import com.csselect.gamification.DailyChallenge;
+import com.csselect.gamification.Streak;
+import com.csselect.user.Player;
+import com.csselect.user.User;
+
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.List;
+
 public class APIFacadePlayer extends APIFacadeUser {
+    private Player player;
     public void register(String email, String password, String param3) {
 
     }
+    public Player getPlayer() {
+        return player;
+    }
+     public Game getGame() {
+        return new Game();
+     }
+
+    /**
+     *
+     * @return All games which this Player participates in
+     */
+     public List<Game> getGames() {
+        return new LinkedList<>();
+     }
+
+     public void acceptInvite(int gameId) {
+
+     }
+
+     public void declineInvite(int gameId) {
+
+     }
+
+     public void startRound(int gameId) {
+
+     }
+
+     public void selectFeatures(Collection<Feature> selected, Collection<Feature> useless) {
+
+     }
+
+
+     public void skipRound(Collection<Feature> features) { // TODO besserer Parametername
+
+     }
+
+     public List<Achievement> getAchievments() {
+         return new LinkedList<>();
+     }
+
+     public List<Player> getLeaderboard() {
+         return new LinkedList<>();
+     }
+
+     public DailyChallenge getDaily() {
+         return new DailyChallenge() {
+             @Override
+             public int hashCode() {
+                 return super.hashCode();
+             }
+         };
+     }
+
+     public Streak getStreak() {
+         return new Streak();
+     }
+
+     public int getScore() {
+         return 0;
+     }
+
+     public String getNotifications() {
+         return "";
+     }
 }
