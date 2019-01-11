746a7a284619b857de25a10dc530b3811f26b05e
Bendix Sonnenberg <bendix.sonnenberg@gmx.de>
2019-01-12 00:22:30 +0100

    Add method for getting single game, login and exception

--- src/main/java/com/csselect/API/httpAPI/Games.java
+++ src/main/java/com/csselect/API/httpAPI/Games.java
@@ -1,16 +1,33 @@
 package com.csselect.API.httpAPI;
-import javax.servlet.http.HttpServlet;
+import com.csselect.API.APIFacadePlayer;
+import com.google.gson.Gson;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
-public class Games extends HttpServlet {
-    @Override
-    public void doGet(HttpServletRequest req, HttpServletResponse resp) {
+public class Games extends Servlet {
+
+
+    public void get(HttpServletRequest req, HttpServletResponse resp) throws HttpError, IOException {
+        /*
+         this method only parses the request string and redirects the call
+         */
+        String requestString = req.getPathInfo();
+        if (requestString.matches("/[0-9]+")) getGame(req, resp);
 
     }
 
     @Override
-    public void doPost(HttpServletRequest req, HttpServletResponse resp) {
+    public void post(HttpServletRequest req, HttpServletResponse resp) {
 
     }
+
+    public void getGame(HttpServletRequest req, HttpServletResponse resp) throws HttpError, IOException{
+        APIFacadePlayer facade = getPlayerFacade();
+        // getting the game id
+        int gameId = Integer.parseInt(req.getPathInfo().substring(1));
+        returnAsJson(resp, facade.getGame(gameId));
+    }
+
 }
--- src/main/java/com/csselect/API/httpAPI/Login.java
+++ src/main/java/com/csselect/API/httpAPI/Login.java
@@ -1,16 +1,44 @@
 package com.csselect.API.httpAPI;
+import com.csselect.API.APIFacadePlayer;
+import com.csselect.database.DatabaseAdapter;
+import com.csselect.database.GameAdapter;
+import com.csselect.database.OrganiserAdapter;
+import com.csselect.database.PlayerAdapter;
+import com.csselect.game.Game;
+import com.csselect.user.Organiser;
+import com.csselect.user.Player;
+
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Collection;
 
-public class Login extends HttpServlet {
+public class Login extends Servlet {
     @Override
     public void doGet(HttpServletRequest req, HttpServletResponse resp) {
+        req.getSession().setAttribute("playerFacade", new APIFacadePlayer());
+    }
+
+    @Override
+    public void get(HttpServletRequest req, HttpServletResponse resp) throws HttpError, IOException {
 
     }
 
     @Override
-    public void doPost(HttpServletRequest req, HttpServletResponse resp) {
+    public void post(HttpServletRequest req, HttpServletResponse resp) throws IOException, HttpError{
+        String email = req.getParameter("email");
+        String password = req.getParameter("password");
+        String organiserSwitch = req.getParameter("organiser");
+
+        if(organiserSwitch.equals("on")) {
+            if (getOrganiserFacade().login(email, password)) resp.sendError(HttpServletResponse.SC_ACCEPTED);
+            else resp.sendError(HttpServletResponse.SC_UNAUTHORIZED);
+        }
+        else {
+            if (getPlayerFacade().login(email, password)) resp.sendError(HttpServletResponse.SC_ACCEPTED);
+            else resp.sendError(HttpServletResponse.SC_UNAUTHORIZED);
+        }
 
     }
 }
