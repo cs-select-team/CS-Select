3de273135c9eaac72aaa61124308035505757bb9
Julian <julian.dinh@web.de>
2019-01-11 22:32:10 +0100

    Add methods to Streak

--- src/main/java/com/csselect/gamification/Streak.java
+++ src/main/java/com/csselect/gamification/Streak.java
@@ -5,4 +5,35 @@ package com.csselect.gamification;
  * plays multiple rounds in a row or not.
  */
 public class Streak {
+
+    private int counter;
+
+    /**
+     * Creates a new Streak by initialising the counter to 0.
+     */
+    public Streak() {
+        this.counter = 0;
+    }
+
+    /**
+     * Sets the counter of the streak back to zero.
+     */
+    public void setZero() {
+        counter = 0;
+    }
+
+    /**
+     * Increases the counter of the streak by 1.
+     */
+    public void increaseStreak() {
+        counter += 1;
+    }
+
+    /**
+     * Gets the current counter of the streak.
+     * @return The current counter.
+     */
+    public int getCounter() {
+        return counter;
+    }
 }
