a6bf63dc1a63063fb97586243b3a9f8eb19a5d77
Luca <luca_springer@yahoo.com>
2019-01-13 20:59:26 +0100

    Add GameCreator methods

--- src/main/java/com/csselect/game/gamecreation/GameCreator.java
+++ src/main/java/com/csselect/game/gamecreation/GameCreator.java
@@ -1,4 +1,26 @@
 package com.csselect.game.gamecreation;
 
+import com.csselect.game.gamecreation.patterns.GameOptions;
+import com.csselect.game.gamecreation.patterns.Pattern;
+import com.csselect.user.Organiser;
+
 public class GameCreator {
+    private Organiser organiser;
+    private GameOptions gameOptions;
+
+    public GameOptions getGameOptions() {
+        return new GameOptions();
+    }
+
+    public void loadPattern(Pattern pattern) {
+
+    }
+
+    public void makePattern(String title) {
+
+    }
+
+    public void doCreate() {
+
+    }
 }
