a4e842ffca5fa43bf582c109905cba823c7f1162
Julian <julian.dinh@web.de>
2019-01-13 20:23:28 +0100

    Add methods to Leaderboard and LeaderboardSortingStrategy
    Add empty SortScoreAllTime and SortScoreLastWeek as well

---
+++ src/main/java/com/csselect/gamification/SortScoreAllTime.java
@@ -0,0 +1,17 @@
+package com.csselect.gamification;
+
+import com.csselect.user.Player;
+
+import java.util.List;
+
+/**
+ * Represent a sorting strategy. It sorts the players according to their points they reached during
+ * last week.
+ */
+public class SortScoreAllTime extends LeaderboardSortingStrategy {
+
+    @Override
+    protected void sort(List<Player> players) {
+
+    }
+}
