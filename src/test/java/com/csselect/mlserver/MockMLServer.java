6d6c5826e171423a8adf53ba8961864483132ce0
Alexander Linder <alexander-linder@gmx.de>
2019-01-13 19:31:47 +0100

    Add Mock-MLServer implementation

---
+++ src/test/java/com/csselect/mlserver/MockMLServer.java
@@ -0,0 +1,35 @@
+package com.csselect.mlserver;
+
+import com.csselect.game.Feature;
+import com.csselect.game.FeatureSet;
+
+import java.util.Collection;
+import java.util.Random;
+
+/**
+ * Mock-Implementation of the {@link com.csselect.mlserver.MLServer} Interface
+ */
+public class MockMLServer implements MLServer {
+
+    private Random random;
+
+    public MockMLServer() {
+        this.random = new Random();
+    }
+
+    @Override
+    public String getVersion() {
+        return "0.1.5";
+    }
+
+    //TODO Populate FeatureSet with Features when the needed classes are available
+    @Override
+    public FeatureSet getFeatures(String dataset) {
+        return new FeatureSet();
+    }
+
+    @Override
+    public double getScore(String dataset, Collection<Feature> selectedFeatures) {
+        return random.nextDouble();
+    }
+}
