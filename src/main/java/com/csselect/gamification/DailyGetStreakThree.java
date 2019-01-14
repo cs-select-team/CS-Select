858fe123da9460bc08603f5c95c70da01763772b
Julian <julian.dinh@web.de>
2019-01-14 20:53:07 +0100

    Add implementation of DailyGetStreakThree
    Date does not work yet

--- src/main/java/com/csselect/gamification/DailyGetStreakThree.java
+++ src/main/java/com/csselect/gamification/DailyGetStreakThree.java
@@ -1,25 +1,42 @@
 package com.csselect.gamification;
 
+import java.util.Date;
+
 /**
  * A daily challenge that will be completed once a player reaches a streak
  * of 3 in one day.
  */
 public class DailyGetStreakThree extends DailyChallenge {
 
+    private int dailyStreak;
+
     /**
      * Creates a daily challenge by setting the necessary values.
      */
     public DailyGetStreakThree() {
-
+        this.name = "";
+        this.description = "";
+        this.date = new Date();
+        this.completed = false;
+        this.reward = 50;
+        this.dailyStreak = 0;
     }
 
     @Override
     public boolean checkFinished(PlayerStats stats) {
-        return false;
+        if (stats.getStreak().getCounter() == 1) {
+            dailyStreak = 1;
+        } else {
+            dailyStreak++;
+        }
+
+        boolean finished = dailyStreak >= 3;
+        return finished;
     }
 
     @Override
     public void resetDaily() {
-
+        completed = false;
+        dailyStreak = 0;
     }
 }
