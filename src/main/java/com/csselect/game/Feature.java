718f45bcc5344d091433db1f606a688efc8648a8
Alexander Linder <alexander-linder@gmx.de>
2019-01-14 12:53:06 +0100

    Add feature method signatures needed for mlserver

--- src/main/java/com/csselect/game/Feature.java
+++ src/main/java/com/csselect/game/Feature.java
@@ -1,4 +1,35 @@
 package com.csselect.game;
 
 public class Feature {
+
+    /**
+     * Gets the {@link Feature}s ID
+     * @return id
+     */
+    public int getID() {
+        return 0;
+    }
+
+    public void setGermanName() {
+
+    }
+
+    public void setEnglishName() {
+
+    }
+    public void setGermanDescription() {
+
+    }
+
+    public void setEnglishDescription() {
+
+    }
+
+    public void setTotalGraph() {
+
+    }
+
+    public void setClassGraph() {
+
+    }
 }
