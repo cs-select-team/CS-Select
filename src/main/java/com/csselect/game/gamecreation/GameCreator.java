f2dc5e3994290dc59d0cdafe1ded54c636c8cb56
Luca <luca_springer@yahoo.com>
2019-01-17 18:55:35 +0100

    Add javadoc to GameCreator

--- src/main/java/com/csselect/game/gamecreation/GameCreator.java
+++ src/main/java/com/csselect/game/gamecreation/GameCreator.java
@@ -5,22 +5,52 @@ import com.csselect.game.gamecreation.patterns.GameOptions;
 import com.csselect.game.gamecreation.patterns.Pattern;
 import com.csselect.user.Organiser;
 
+/**
+ * This class is a builder class for a game {@link Game} object. An organiser is able to create a game, so in order to
+ * not use a constructor with a long list of parameters we use setters and getters to do the trick.
+ * GameCreator is used by an {@link Organiser} object to load settings he already chose or create a new game object.
+ */
 public class GameCreator {
-    private Organiser organiser;
     private GameOptions gameOptions;
 
+    /**
+     * Getter for the {@link GameOptions} attribute
+     * @return GameOptions
+     */
     public GameOptions getGameOptions() {
-        return new GameOptions();
+        return gameOptions;
     }
 
+    /**
+     * Set a game option using Strings as parameters.
+     * @param option Option of {@link GameOptions} which are set
+     * @param data Value of the option
+     */
+    public void setOption(String option, String data) {
+
+    }
+
+    /**
+     * Load a pattern into the GameCreator
+     * @param pattern {@link Pattern} object which is to load
+     */
     public void loadPattern(Pattern pattern) {
         this.gameOptions = pattern.getGameOptions();
     }
 
+    /**
+     * Create a new {@link Pattern} object containing {@link GameOptions} attribute
+     * @param title Title of the pattern
+     * @return {@link Pattern} object
+     */
     public Pattern makePattern(String title) {
         return new Pattern(gameOptions, title);
     }
 
+    /**
+     * Method to create a new game using the selected game options ({@link GameOptions}
+     * @return New {@link Game} object
+     */
     public Game doCreate() {
         return null;
     }
