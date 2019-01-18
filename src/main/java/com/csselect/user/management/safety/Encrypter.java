4ad90f13ed383dbfac993ad495d4d5eb94de9d57
Luca <luca_springer@yahoo.com>
2019-01-18 17:57:55 +0100

    Implement whole management package and safety package

--- src/main/java/com/csselect/user/management/safety/Encrypter.java
+++ src/main/java/com/csselect/user/management/safety/Encrypter.java
@@ -5,15 +5,28 @@ import org.apache.commons.lang3.RandomStringUtils;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
+/**
+ * This class is there to encrypt Strings using an specified algorithm and provide a method to get an
+ * randomised String ("Salt") for safety measures.
+ */
 public class Encrypter {
     private final static String ALGORITHM = "SHA-256";
     private final static int MIN_SALT_LENGTH = 40;
     private final static int MAX_SALT_LENGTH = 70;
 
+    /**
+     * Generates a random alphanumeric String with a specified length
+     * @return Random String (used as salt)
+     */
     public static String getRandomSalt() {
         return RandomStringUtils.randomAlphanumeric(MIN_SALT_LENGTH, MAX_SALT_LENGTH);
     }
 
+    /**
+     * Encrypts a String using an specified algorithm
+     * @param word String to encrypt
+     * @return Encrypted String
+     */
     public static String encrypt(String word) {
         MessageDigest encryption;
         try {
