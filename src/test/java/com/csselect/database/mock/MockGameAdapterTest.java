12db959b7f79a7028fed7fc2232abb8232b13fea
Alexander Linder <alexander-linder@gmx.de>
2019-01-18 20:01:49 +0100

    Add MockGameAdapter tests

---
+++ src/test/java/com/csselect/database/mock/MockGameAdapterTest.java
@@ -0,0 +1,100 @@
+package com.csselect.database.mock;
+
+import com.csselect.Injector;
+import com.csselect.TestClass;
+import com.csselect.database.DatabaseAdapter;
+import com.csselect.database.GameAdapter;
+import com.csselect.user.Player;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Collection;
+import java.util.HashSet;
+
+public class MockGameAdapterTest extends TestClass {
+
+    private static final String TEST_TITLE = "PSE - Projekt Studenten exmatrikulieren";
+    private static final String TEST_DESCRIPTION = "This game's aim is to find out which of students traits correlate with failure in the PSE";
+    private static final String TEST_DATABASE_NAME = "PSE";
+    private static final String TEST_EMAIL = "test@test.de";
+    private static final String TEST_EMAIL2 = "test2@test.de";
+
+    private GameAdapter adapter;
+    private MockDatabaseAdapter mockDatabaseAdapter;
+
+    @Override
+    public void setUp() {
+        adapter = new MockGameAdapter(0);
+        mockDatabaseAdapter = (MockDatabaseAdapter) Injector.getInjector().getInstance(DatabaseAdapter.class);
+    }
+
+    @Override
+    public void reset() {
+
+    }
+
+    @Test
+    public void testGetID() {
+        Assert.assertEquals(0, adapter.getID());
+    }
+
+    @Test
+    public void testTitle() {
+        adapter.setTitle(TEST_TITLE);
+        Assert.assertEquals(TEST_TITLE, adapter.getTitle());
+    }
+
+    @Test
+    public void testDescription() {
+        adapter.setDescription(TEST_DESCRIPTION);
+        Assert.assertEquals(TEST_DESCRIPTION, adapter.getDescription());
+    }
+
+    @Test
+    public void testDatabaseName() {
+        adapter.setDatabase(TEST_DATABASE_NAME);
+        Assert.assertEquals(TEST_DATABASE_NAME, adapter.getDatabaseName());
+    }
+
+    @Test
+    public void testFinished() {
+        Assert.assertFalse(adapter.isFinished());
+        adapter.setFinished();
+        Assert.assertTrue(adapter.isFinished());
+    }
+
+    @Test
+    public void testPlayerInvitation() {
+        Collection<String> emails = new HashSet<>();
+        emails.add(TEST_EMAIL);
+        emails.add(TEST_EMAIL2);
+        adapter.addInvitedPlayers(emails);
+        Assert.assertEquals(emails, adapter.getInvitedPlayers());
+    }
+
+    @Test
+    public void testPlayingPlayerAdding() {
+        Collection<String> emails = new HashSet<>();
+        Player player1 = mockDatabaseAdapter.createPlayer(TEST_EMAIL, TEST_EMAIL, TEST_EMAIL, TEST_EMAIL);
+        Player player2 = mockDatabaseAdapter.createPlayer(TEST_EMAIL2, TEST_EMAIL, TEST_EMAIL, TEST_EMAIL2);
+        emails.add(TEST_EMAIL);
+        emails.add(TEST_EMAIL2);
+        adapter.addPlayingPlayers(emails);
+        Collection<Player> players = adapter.getPlayingPlayers();
+        Assert.assertTrue(players.contains(player1));
+        Assert.assertTrue(players.contains(player2));
+    }
+
+    @Test
+    public void testInvitedPlayerRemoval() {
+        Collection<String> emails = new HashSet<>();
+        Collection<String> toRemove = new HashSet<>();
+        emails.add(TEST_EMAIL);
+        emails.add(TEST_EMAIL2);
+        toRemove.add(TEST_EMAIL2);
+        adapter.addInvitedPlayers(emails);
+        adapter.removeInvitedPlayers(toRemove);
+        Assert.assertTrue(adapter.getInvitedPlayers().contains(TEST_EMAIL));
+        Assert.assertFalse(adapter.getInvitedPlayers().contains(TEST_EMAIL2));
+    }
+}
