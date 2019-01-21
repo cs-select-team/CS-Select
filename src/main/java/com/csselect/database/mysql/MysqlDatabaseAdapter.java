88dbfa7255bc8b5fe58f8a8a8eb0480049b93e4b
Alexander Linder <alexander-linder@gmx.de>
2019-01-21 19:27:38 +0100

    Implement most methods of MysqlDatabaseAdapter

--- src/main/java/com/csselect/database/mysql/MysqlDatabaseAdapter.java
+++ src/main/java/com/csselect/database/mysql/MysqlDatabaseAdapter.java
@@ -18,6 +18,7 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Collection;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 
 /**
@@ -33,12 +34,15 @@ public class MysqlDatabaseAdapter implements DatabaseAdapter {
     private final String password;
     private final Map<String, MysqlDataSource> dataSources;
 
+    private final Map<Game, Organiser> gameMap;
+
     /**
      * Creates a new MysqlDatabaseAdapter
      * @param configuration configuration to use
      */
     @Inject
     MysqlDatabaseAdapter(Configuration configuration) {
+        gameMap = new HashMap<>();
         this.hostname = configuration.getDatabaseHostname();
         this.port = configuration.getDatabasePort();
         this.username = configuration.getDatabaseUsername();
@@ -59,82 +63,218 @@ public class MysqlDatabaseAdapter implements DatabaseAdapter {
 
     @Override
     public PlayerAdapter getPlayerAdapter(int id) {
-        return null;
+        return new MysqlPlayerAdapter(id);
     }
 
     @Override
     public OrganiserAdapter getOrganiserAdapter(int id) {
-        return null;
+        return new MysqlOrganiserAdapter(id);
     }
 
     @Override
     public GameAdapter getGameAdapter(int id) {
-        return null;
+        return new MysqlGameAdapter(id);
     }
 
     @Override
     public Player getPlayer(String email) {
-        return null;
+        try {
+            ResultSet set = executeMysqlQuery("SELECT * FROM players WHERE (email='" + email + "');");
+            return getPlayer(set);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 
     @Override
     public Player getPlayer(int id) {
-        return null;
+        try {
+            ResultSet set = executeMysqlQuery("SELECT * FROM players WHERE (id='" + id + "';");
+            return getPlayer(set);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    private Player getPlayer(ResultSet set) throws SQLException {
+        if (set.next()) {
+            Player p = new Player(new MysqlPlayerAdapter(set.getInt("id")));
+            set.close();
+            return p;
+        } else {
+            return null;
+        }
     }
 
     @Override
     public Organiser getOrganiser(String email) {
-        return null;
+        try {
+            ResultSet set = executeMysqlQuery("SELECT * FROM organisers WHERE (email='" + email + "');");
+            if (set.next()) {
+                Organiser o = new Organiser(new MysqlOrganiserAdapter(set.getInt("id")));
+                set.close();
+                return o;
+            } else {
+                return null;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 
     @Override
     public Collection<Player> getPlayers() {
-        return null;
+        Collection<Player> players = new HashSet<>();
+        try {
+            ResultSet set = executeMysqlQuery("SELECT * FROM players");
+            while (set.next()) {
+                players.add(new Player(new MysqlPlayerAdapter(set.getInt("id"))));
+            }
+            set.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return players;
     }
 
     @Override
     public int getNextGameID() {
-        return 0;
+        try {
+            return getNextIdOfTable("games");
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return 1;
+        }
     }
 
     @Override
     public String getPlayerHash(int id) {
-        return null;
+        try {
+            ResultSet set = executeMysqlQuery("SELECT hash AS hash FROM players WHERE (id='" + id + "');");
+            set.next();
+            String hash = set.getString("hash");
+            set.close();
+            return hash;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 
     @Override
     public String getPlayerSalt(int id) {
-        return null;
+        try {
+            ResultSet set = executeMysqlQuery("SELECT salt AS salt FROM players WHERE (id='" + id + "');");
+            set.next();
+            String salt = set.getString("salt");
+            set.close();
+            return salt;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 
     @Override
     public String getOrganiserHash(int id) {
-        return null;
+        try {
+            ResultSet set = executeMysqlQuery("SELECT hash AS hash FROM organisers WHERE (id='" + id + "');");
+            set.next();
+            String hash = set.getString("hash");
+            set.close();
+            return hash;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 
     @Override
     public String getOrganiserSalt(int id) {
-        return null;
+        try {
+            ResultSet set = executeMysqlQuery("SELECT salt AS salt FROM organisers WHERE (id='" + id + "');");
+            set.next();
+            String salt = set.getString("salt");
+            set.close();
+            return salt;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 
     @Override
     public Player createPlayer(String email, String hash, String salt, String username) {
-        return null;
+        try {
+            ResultSet set = executeMysqlQuery("SELECT * FROM players WHERE (email='" + email + "');");
+            if (set.next()) {
+                return null;
+            }
+            set.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+        MysqlPlayerAdapter adapter;
+        try {
+            adapter = new MysqlPlayerAdapter();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+        adapter.setEmail(email);
+        adapter.setPassword(hash, salt);
+        adapter.setUsername(username);
+        return new Player(adapter);
     }
 
     @Override
     public Organiser createOrganiser(String email, String hash, String salt) {
-        return null;
+        try {
+            ResultSet set = executeMysqlQuery("SELECT * FROM organisers WHERE (email='" + email + "');");
+            if (set.next()) {
+                return null;
+            }
+            set.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+        MysqlOrganiserAdapter adapter;
+        try {
+            adapter = new MysqlOrganiserAdapter();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+        adapter.setEmail(email);
+        adapter.setPassword(hash, salt);
+        return new Organiser(adapter);
     }
 
     @Override
     public void registerGame(Organiser organiser, Game game) {
-
+        if (!gameMap.containsKey(game)) {
+            gameMap.put(game, organiser);
+            try {
+                executeMysqlUpdate("UPDATE organiser_id SET " + organiser.getId() + " FROM games WHERE"
+                        + " (id='" + game.getId() + "');");
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     @Override
     public void removeGame(Game game) {
-
+        try {
+            executeMysqlUpdate("DELETE FROM games WHERE (id='" + game.getId() + "');");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     /**
@@ -158,8 +298,7 @@ public class MysqlDatabaseAdapter implements DatabaseAdapter {
         MysqlDataSource dataSource = dataSources.getOrDefault(databaseName, createDataSource(databaseName));
         Connection connection = dataSource.getConnection();
         Statement statement = connection.createStatement();
-        ResultSet resultSet = statement.executeQuery(query);
-        return resultSet;
+        return statement.executeQuery(query);
     }
 
     /**
@@ -201,5 +340,20 @@ public class MysqlDatabaseAdapter implements DatabaseAdapter {
         }
         dataSources.put(databaseName, source);
         return source;
+    }
+
+    /**
+     * Gets the next id an object from the given table will have
+     * @param tableName name of the table to get the next id from
+     * @return next id
+     * @throws SQLException Thrown if an error occurs while communicating with the database
+     */
+    int getNextIdOfTable(String tableName) throws SQLException {
+        ResultSet set = executeMysqlQuery("SELECT MAX(id) AS id FROM " + tableName + ";");
+        if (!set.next()) {
+            return 1;
+        } else {
+            return set.getInt("id") + 1;
+        }
     }
 }
