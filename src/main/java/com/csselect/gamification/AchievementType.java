ce2a3fefe6dc67c1ee40805d3ee2a8b0ba27921f
Julian <julian.dinh@web.de>
2019-01-24 17:15:35 +0100

    Implement all instances of AchievementType

--- src/main/java/com/csselect/gamification/AchievementType.java
+++ src/main/java/com/csselect/gamification/AchievementType.java
@@ -2,7 +2,7 @@ package com.csselect.gamification;
 
 /**
  * An enumeration for all the existing achievements. Every instance has to implement the method
- * checkProgress.
+ * getState.
  */
 public enum AchievementType {
 
@@ -133,91 +133,198 @@ public enum AchievementType {
     DAILY_ONE  {
         @Override
         protected AchievementState getState(PlayerStats stats) {
-            return null;
+            if (stats.getDailiesCompleted() >= 1) {
+                return AchievementState.FINISHED;
+            }
+            return AchievementState.SHOWN;
         }
     },
 
     DAILY_THREE  {
         @Override
         protected AchievementState getState(PlayerStats stats) {
-            return null;
+            if (stats.getDailiesCompleted() >= 3) {
+                return AchievementState.FINISHED;
+            }
+
+            if (stats.getDailiesCompleted() >= 1) {
+                return AchievementState.SHOWN;
+            }
+            return AchievementState.CONCEALED;
         }
     },
 
     DAILY_SEVEN  {
         @Override
         protected AchievementState getState(PlayerStats stats) {
-            return null;
+            if (stats.getDailiesCompleted() >= 7) {
+                return AchievementState.FINISHED;
+            }
+
+            if (stats.getDailiesCompleted() >= 3) {
+                return AchievementState.SHOWN;
+            }
+
+            if (stats.getDailiesCompleted() >= 1) {
+                return AchievementState.CONCEALED;
+            }
+            return AchievementState.INVISIBLE;
         }
     },
 
     TOTAL_SCORE_HUNDRED  {
         @Override
         protected AchievementState getState(PlayerStats stats) {
-            return null;
+            if (stats.getScore() >= 100) {
+                return AchievementState.FINISHED;
+            }
+            return AchievementState.SHOWN;
         }
     },
 
     TOTAL_SCORE_TWOHUNDREDFIFTY  {
         @Override
         protected AchievementState getState(PlayerStats stats) {
-            return null;
+            if (stats.getScore() >= 250) {
+                return AchievementState.FINISHED;
+            }
+
+            if (stats.getScore() >= 100) {
+                return AchievementState.SHOWN;
+            }
+            return AchievementState.CONCEALED;
         }
     },
 
     TOTAL_SCORE_FIVEHUNDRED  {
         @Override
         protected AchievementState getState(PlayerStats stats) {
-            return null;
+            if (stats.getScore() >= 500) {
+                return AchievementState.FINISHED;
+            }
+
+            if (stats.getScore() >= 250) {
+                return AchievementState.SHOWN;
+            }
+
+            if (stats.getScore() >= 100) {
+                return AchievementState.CONCEALED;
+            }
+            return AchievementState.INVISIBLE;
         }
     },
 
     TOTAL_SCORE_THOUSAND  {
         @Override
         protected AchievementState getState(PlayerStats stats) {
-            return null;
+            if (stats.getScore() >= 1000) {
+                return AchievementState.FINISHED;
+            }
+
+            if (stats.getScore() >= 500) {
+                return AchievementState.SHOWN;
+            }
+
+            if (stats.getScore() >= 250) {
+                return AchievementState.CONCEALED;
+            }
+            return AchievementState.INVISIBLE;
         }
     },
 
     TOTAL_SCORE_TWOTHOUSAND  {
         @Override
         protected AchievementState getState(PlayerStats stats) {
-            return null;
+            if (stats.getScore() >= 2000) {
+                return AchievementState.FINISHED;
+            }
+
+            if (stats.getScore() >= 1000) {
+                return AchievementState.SHOWN;
+            }
+
+            if (stats.getScore() >= 500) {
+                return AchievementState.CONCEALED;
+            }
+            return AchievementState.INVISIBLE;
         }
     },
 
     TOTAL_SCORE_FIVETHOUSAND  {
         @Override
         protected AchievementState getState(PlayerStats stats) {
-            return null;
+            if (stats.getScore() >= 5000) {
+                return AchievementState.FINISHED;
+            }
+
+            if (stats.getScore() >= 2000) {
+                return AchievementState.SHOWN;
+            }
+
+            if (stats.getScore() >= 1000) {
+                return AchievementState.CONCEALED;
+            }
+            return AchievementState.INVISIBLE;
         }
     },
 
     ROUND_SCORE_SIXTY  {
         @Override
         protected AchievementState getState(PlayerStats stats) {
-            return null;
+            if (stats.getMaxRoundScore() >= 60) {
+                return AchievementState.FINISHED;
+            }
+            return AchievementState.SHOWN;
         }
     },
 
     ROUND_SCORE_SEVENTY  {
         @Override
         protected AchievementState getState(PlayerStats stats) {
-            return null;
+            if (stats.getMaxRoundScore() >= 70) {
+                return AchievementState.FINISHED;
+            }
+
+            if (stats.getMaxRoundScore() >= 60) {
+                return AchievementState.SHOWN;
+            }
+            return AchievementState.CONCEALED;
         }
     },
 
     ROUND_SCORE_EIGHTY  {
         @Override
         protected AchievementState getState(PlayerStats stats) {
-            return null;
+            if (stats.getMaxRoundScore() >= 80) {
+                return AchievementState.FINISHED;
+            }
+
+            if (stats.getMaxRoundScore() >= 70) {
+                return AchievementState.SHOWN;
+            }
+
+            if (stats.getMaxRoundScore() >= 60) {
+                return AchievementState.CONCEALED;
+            }
+            return AchievementState.INVISIBLE;
         }
     },
 
     ROUND_SCORE_NINETY  {
         @Override
         protected AchievementState getState(PlayerStats stats) {
-            return null;
+            if (stats.getMaxRoundScore() >= 90) {
+                return AchievementState.FINISHED;
+            }
+
+            if (stats.getMaxRoundScore() >= 80) {
+                return AchievementState.SHOWN;
+            }
+
+            if (stats.getMaxRoundScore() >= 70) {
+                return AchievementState.CONCEALED;
+            }
+            return AchievementState.INVISIBLE;
         }
     };
 
