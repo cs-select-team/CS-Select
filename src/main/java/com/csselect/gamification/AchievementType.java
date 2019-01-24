e0797107bd6c781210148f01215d5c36cc7ada9b
Julian <julian.dinh@web.de>
2019-01-24 15:00:36 +0100

    Add abstract method getState and make checkProgress not abstract
    This is now a template method.

--- src/main/java/com/csselect/gamification/AchievementType.java
+++ src/main/java/com/csselect/gamification/AchievementType.java
@@ -8,151 +8,156 @@ public enum AchievementType {
 
     PLAY_ROUND_ONE {
         @Override
-        public Achievement checkProgress(PlayerStats stats) {
+        protected AchievementState getState(PlayerStats stats) {
             return null;
         }
     },
 
     PLAY_ROUND_FIVE {
         @Override
-        public Achievement checkProgress(PlayerStats stats) {
+        protected AchievementState getState(PlayerStats stats) {
             return null;
         }
     },
 
     PLAY_ROUND_TEN  {
         @Override
-        public Achievement checkProgress(PlayerStats stats) {
+        protected AchievementState getState(PlayerStats stats) {
             return null;
         }
     },
 
     PLAY_ROUND_FOURTYTWO  {
         @Override
-        public Achievement checkProgress(PlayerStats stats) {
+        protected AchievementState getState(PlayerStats stats) {
             return null;
         }
     },
 
     PLAY_ROUND_HUNDRED  {
         @Override
-        public Achievement checkProgress(PlayerStats stats) {
+        protected AchievementState getState(PlayerStats stats) {
             return null;
         }
     },
 
     STREAK_TWO  {
         @Override
-        public Achievement checkProgress(PlayerStats stats) {
+        protected AchievementState getState(PlayerStats stats) {
             return null;
         }
     },
 
     STREAK_FIVE  {
         @Override
-        public Achievement checkProgress(PlayerStats stats) {
+        protected AchievementState getState(PlayerStats stats) {
             return null;
         }
     },
 
     STREAK_TEN  {
         @Override
-        public Achievement checkProgress(PlayerStats stats) {
+        protected AchievementState getState(PlayerStats stats) {
             return null;
         }
     },
 
     DAILY_ONE  {
         @Override
-        public Achievement checkProgress(PlayerStats stats) {
+        protected AchievementState getState(PlayerStats stats) {
             return null;
         }
     },
 
     DAILY_THREE  {
         @Override
-        public Achievement checkProgress(PlayerStats stats) {
+        protected AchievementState getState(PlayerStats stats) {
             return null;
         }
     },
 
     DAILY_SEVEN  {
         @Override
-        public Achievement checkProgress(PlayerStats stats) {
+        protected AchievementState getState(PlayerStats stats) {
             return null;
         }
     },
 
     TOTAL_SCORE_HUNDRED  {
         @Override
-        public Achievement checkProgress(PlayerStats stats) {
+        protected AchievementState getState(PlayerStats stats) {
             return null;
         }
     },
 
     TOTAL_SCORE_TWOHUNDREDFIFTY  {
         @Override
-        public Achievement checkProgress(PlayerStats stats) {
+        protected AchievementState getState(PlayerStats stats) {
             return null;
         }
     },
 
     TOTAL_SCORE_FIVEHUNDRED  {
         @Override
-        public Achievement checkProgress(PlayerStats stats) {
+        protected AchievementState getState(PlayerStats stats) {
             return null;
         }
     },
 
     TOTAL_SCORE_THOUSAND  {
         @Override
-        public Achievement checkProgress(PlayerStats stats) {
+        protected AchievementState getState(PlayerStats stats) {
             return null;
         }
     },
 
     TOTAL_SCORE_TWOTHOUSAND  {
         @Override
-        public Achievement checkProgress(PlayerStats stats) {
+        protected AchievementState getState(PlayerStats stats) {
             return null;
         }
     },
 
     TOTAL_SCORE_FIVETHOUSAND  {
         @Override
-        public Achievement checkProgress(PlayerStats stats) {
+        protected AchievementState getState(PlayerStats stats) {
             return null;
         }
     },
 
     ROUND_SCORE_SIXTY  {
         @Override
-        public Achievement checkProgress(PlayerStats stats) {
+        protected AchievementState getState(PlayerStats stats) {
             return null;
         }
     },
 
     ROUND_SCORE_SEVENTY  {
         @Override
-        public Achievement checkProgress(PlayerStats stats) {
+        protected AchievementState getState(PlayerStats stats) {
             return null;
         }
     },
 
     ROUND_SCORE_EIGHTY  {
         @Override
-        public Achievement checkProgress(PlayerStats stats) {
+        protected AchievementState getState(PlayerStats stats) {
             return null;
         }
     },
 
     ROUND_SCPRE_NINETY  {
         @Override
-        public Achievement checkProgress(PlayerStats stats) {
+        protected AchievementState getState(PlayerStats stats) {
             return null;
         }
     };
 
-    public abstract Achievement checkProgress(PlayerStats stats);
+    public final Achievement checkProgress(PlayerStats stats) {
+        AchievementState s = getState(stats);
+        return new Achievement(s, this);
+    }
+
+    protected abstract AchievementState getState(PlayerStats stats);
 
 }
