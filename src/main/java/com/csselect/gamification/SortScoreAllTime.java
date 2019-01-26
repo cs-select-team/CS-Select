d163c06a398118f35c1e6b16e1f62c9a8888e01f
Julian <julian.dinh@web.de>
2019-01-26 16:42:15 +0100

    Fix SortScoreAllTime
    This now works correctly. Added SortScoreAllTimeTests.

--- src/main/java/com/csselect/gamification/SortScoreAllTime.java
+++ src/main/java/com/csselect/gamification/SortScoreAllTime.java
@@ -2,27 +2,26 @@ package com.csselect.gamification;
 
 import com.csselect.user.Player;
 
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
 
 /**
- * Represent a sorting strategy. It sorts the players according to their points they reached during
- * last week.
+ * Represent a sorting strategy. It sorts the players according to their points they
+ * reached all time.
  */
 public class SortScoreAllTime extends LeaderboardSortingStrategy {
 
     @Override
     protected Map<Player, Integer> sort(List<Player> players) {
-        // Collections.sort(players, Comparator.comparing(p -> p.getStats().getScore()));
 
-        Map<Player, Integer> sortAllTime = new LinkedHashMap<>();
-        players.stream().forEach(p -> sortAllTime.put(p, p.getStats().getScore()));
+        Map<Player, Integer> sortAllTime = new TreeMap<>();
+        for (Player player : players) {
+            sortAllTime.put(player, player.getStats().getScore());
+        }
 
         Map<Player, Integer> sortedMap = sortAllTime.entrySet().stream()
-                .sorted(Map.Entry.comparingByValue())
-                .collect(Collectors.toMap(e -> e.getKey(), e -> e.getValue(), (e1, e2) -> e1));
+                .sorted(Collections.reverseOrder(Comparator.comparing(Map.Entry::getValue)))
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e1, LinkedHashMap::new));
 
         return sortedMap;
     }
