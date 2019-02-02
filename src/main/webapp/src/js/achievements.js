78565a9b123f48460d5e30e2f7f6cb8c8546156d
Bendix Sonnenberg <bendix.sonnenberg@gmx.de>
2019-02-02 18:40:27 +0100

    Change property name

--- src/main/webapp/src/js/achievements.js
+++ src/main/webapp/src/js/achievements.js
@@ -2,7 +2,7 @@ Vue.component('achievement',{
     props: ['achievement'],
 
     template: '    <div class="card" v-if="achievement.state > 0">\n' +
-        '        <h5 class="card-title"  v-if="achievement.state > 1">{{ achievement.title }}</h5>\n' +
+        '        <h5 class="card-title"  v-if="achievement.state > 1">{{ achievement.name }}</h5>\n' +
         '        <div  v-if="achievement.state > 2"class="card-body">\n' +
         '            {{ achievement.desc }}\n' +
         '        </div>\n' +
@@ -11,7 +11,7 @@ Vue.component('achievement',{
 })
 
 /*  state   description
-    0       invisiable
+    0       invisible
     1       hidden( the box is there, but nothing else)
     2       shown
     3       completed
@@ -19,26 +19,7 @@ Vue.component('achievement',{
 var achievements = new Vue({
     el: '#achievements',
     data: {
-        achievementList: [
-            {state: 0, desc:'click the like button 500 times', title: 'moron', id: 0},
-            {state: 1, desc:'click the like button 500 times', title: 'moron', id: 1},
-            {state: 2, desc:'click the like button 500 times', title: 'moron', id: 2},
-            {state: 2, desc:'click the like button 500 times', title: 'moron', id: 2},
-            {state: 2, desc:'click the like button 500 times', title: 'moron', id: 2},
-            {state: 2, desc:'click the like button 500 times', title: 'moron', id: 2},
-            {state: 2, desc:'click the like button 500 times', title: 'moron', id: 2},
-            {state: 2, desc:'click the like button 500 times', title: 'moron', id: 2},
-            {state: 2, desc:'click the like button 500 times', title: 'moron', id: 2},
-            {state: 3, desc:'click the like button 500 times', title: 'moron', id: 3},
-            {state: 3, desc:'click the like button 500 times', title: 'moron', id: 3},
-            {state: 3, desc:'click the like button 500 times', title: 'moron', id: 3},
-            {state: 3, desc:'click the like button 500 times', title: 'moron', id: 3},
-            {state: 3, desc:'click the like button 500 times', title: 'moron', id: 3},
-            {state: 3, desc:'click the like button 500 times', title: 'moron', id: 3},
-            {state: 3, desc:'click the like button 500 times', title: 'moron', id: 3},
-            {state: 3, desc:'click the like button 500 times', title: 'moron', id: 3},
-            {state: 3, desc:'click the like button 500 times', title: 'moron', id: 3},
-            {state: 3, desc:'click the like button 500 times', title: 'moron', id: 3}]
+        achievementList: []
 
     },
     mounted: function () {
