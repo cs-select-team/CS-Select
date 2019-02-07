c8f2f4648653b1f599d84fed479d34236b156251
Bendix Sonnenberg <bendix.sonnenberg@gmx.de>
2019-02-07 15:03:41 +0100

    Add title field and new way of adding players to the gamecreation

--- src/main/webapp/gamecreation.jsp
+++ src/main/webapp/gamecreation.jsp
@@ -1,9 +1,28 @@
-    <%@ include file="WEB-INF/jspf/header.jspf" %>
-        <fmt:bundle basename="locale.Locale">
-            <div>
-            <%@ include file="WEB-INF/jspf/organiserNavbar.jspf" %>
+<%@ include file="WEB-INF/jspf/header.jspf" %>
+<fmt:bundle basename="locale.Locale">
+    <div>
+        <%@ include file="WEB-INF/jspf/organiserNavbar.jspf" %>
+        <div class="container" id="gamecreation">
+            <div class="row mt-2">
+                <div class="col-4">
+                    <div class="input-group mb-3">
+                        <input type="text" class="form-control" placeholder="Name" aria-label="Name" v-model="name">
+                    </div>
 
+                    <ul class="nav nav-tabs">
+                        <player-invite-nav-tab v-for="(value, index) in playerInputType" v-bind:key="index"
+                                               v-bind:title="value.title" v-bind:value="value.value"
+                                                v-on:update-tab="updateTab">
+
+                        </player-invite-nav-tab>
+                    </ul>
+                    <component v-bind:is="currentTabComponent" v-on:new-email="addInvitedPlayer" v-bind:invited-players="invitedPlayers"
+                                          v-on:update-invited="updateInvited"></component>
+                </div>
             </div>
-            <script src="src/js/gamecreation.js"></script>
-        </fmt:bundle>
-        <%@ include file="WEB-INF/jspf/footer.jspf" %>
+
+        </div>
+    </div>
+    <script src="src/js/gamecreation.js"></script>
+</fmt:bundle>
+<%@ include file="WEB-INF/jspf/footer.jspf" %>
--- src/main/webapp/src/js/gamecreation.js
+++ src/main/webapp/src/js/gamecreation.js
@@ -1,12 +1,93 @@
-Vue.component('text-input',{
-    props: ['label'],
-    template: '<'
-})
+Vue.component('player-invite-single', {
+    data: function () {
+        return {
+            email: ''
+        }
+    },
 
+    template: '<div class="input-group mb-3">\n' +
+        '  <input type="email" class="form-control" placeholder="" aria-label="" ' +
+        '       v-model="email" aria-describedby="inviteButton">\n' +
+        '  <div class="input-group-append">\n' +
+        '    <button class="btn btn-outline-secondary" type="button" :disabled="!this.validateEmail"' +
+        ' v-on:click="newEmail" id="inviteButton">Button</button>\n' +
+        '  </div>\n' +
+        '</div>',
+
+    methods: {
+        newEmail: function () {
+            this.$emit('new-email', this.email)
+            this.email = "";
+        }
+    },
+    computed: {
+        validateEmail: function () {
+            var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+            return re.test(String(this.email).toLowerCase());
+        }
+    }
+})
+Vue.component('player-invite-textarea', {
+    props: ['invited-players'],
+    data: function () {
+        return {
+            rawText: this.joinedWith(this.invitedPlayers, ',')
+        }
+    },
+    template: '<div class="input-group">\n' +
+        '  <textarea class="form-control" aria-label="" v-model="rawText"></textarea>\n' +
+        '</div>',
+    methods: {
+        joinedWith: function (array, char) {
+            var string = '';
+            array.forEach(function (value, index) {
+                string += value;
+                if (index < array.length - 1) {
+                    string += char
+                }
+            })
+            return string;
+        },
+        validateEmail: function (email) {
+            var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+            return re.test(String(email).toLowerCase());
+        }
+    },
+    watch: {
+        rawText: function (newVal) {
+            var self = this;
+            var wrongEmail = false;
+            newVal = newVal.replace(/\s/g, '');
+            var array = newVal.split(',');
+            array.forEach(function (value) {
+                if (value == '') return // trailing comma will cause one empty string
+                if (!self.validateEmail(value)) {
+                    wrongEmail = true;
+                }
+            });
+            if (wrongEmail) {
+                // TODO inform user
+            } else {
+                this.$emit('update-invited', array);
+            }
+        }
+    }
+});
+Vue.component('player-invite-nav-tab', {
+    props: ['title', 'value', 'current-tab'],
+    template: '<li class="nav-item">\n' +
+        '                            <a class="nav-link" v-bind:class="{active: currentTab == value}"\n' +
+        '                                v-on:click="$emit(\'update-tab\', value)" href="#" v-on>{{title}}</a>\n' +
+        '                        </li>'
+})
 var creation = new Vue({
     el: '#gamecreation',
     data: {
         gamemodeOptions: {},
+        invitedPlayers: [],
+        playerInputType: [{title: 'single', value: 'single'}, {title: 'textarea', value: 'textarea'}],
+        name: '',
+        currentTab: 'single'
 
     },
     methods: {
@@ -15,7 +96,7 @@ var creation = new Vue({
          * @param name name of the parameter
          * @param value value of that parameter
          */
-        submitParameter: function(name, value) {
+        submitParameter: function (name, value) {
             axios({
                 method: 'post',
                 url: 'create/setParam',
@@ -24,6 +105,21 @@ var creation = new Vue({
                     value: value
                 }
             })
+        },
+        addInvitedPlayer: function (email) {
+            this.invitedPlayers.push(email);
+        },
+        updateInvited: function (array) {
+
+            this.invitedPlayers = array;
+        },
+        updateTab: function(newVal) {
+            this.currentTab = newVal;
+        }
+    },
+    computed: {
+        currentTabComponent: function () {
+            return 'player-invite-' + this.currentTab
         }
     }
 });
