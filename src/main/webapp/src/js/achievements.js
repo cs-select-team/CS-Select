52fb995f408aedaa14dde0d59dd1dfa616116c95
Bendix Sonnenberg <bendix.sonnenberg@gmx.de>
2019-02-08 15:57:45 +0100

    Add green background and completed message if a achievement has been completed
    resolves #97

--- src/main/webapp/src/js/achievements.js
+++ src/main/webapp/src/js/achievements.js
@@ -3,11 +3,12 @@ Vue.component('achievement',{
 
     template:
         '<div class="card" v-if="achievement.state > 0">\n' +
-        '  <div class="card-body">\n' +
+        '  <div class="card-body" v-bind:class="{ \'bg-success\': achievement.state > 2}">\n' +
         '    <h5 class="card-title"  v-if="achievement.state > 1">{{ achievement.name }}</h5>\n' +
-        '    <div  v-if="achievement.state > 2"class="card-text">\n' +
+        '    <div  v-if="achievement.state > 1"class="card-text">\n' +
         '      {{ achievement.desc }}\n' +
-        '    </div>\n' +
+        '    </div>' +
+        '    <div class="card-text" v-if="achievement.state > 2"> {{localisation.completed}}</div>' +
         '  </div>\n' +
         '</div>'
 
