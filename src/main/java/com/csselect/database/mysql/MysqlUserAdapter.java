f0c248f824e6d838e80b26915b15f99b140004a3
Alexander Linder <alexander-linder@gmx.de>
2019-02-10 15:19:46 +0100

    Make UserAdapter throw IllegalArgumentException
    If the email is already in use, or an error occurs checking if it is, an IllegalArgumentException is thrown to prevent duplicate emails

--- src/main/java/com/csselect/database/mysql/MysqlUserAdapter.java
+++ src/main/java/com/csselect/database/mysql/MysqlUserAdapter.java
@@ -45,7 +45,17 @@ public abstract class MysqlUserAdapter extends MysqlAdapter implements UserAdapt
     }
 
     @Override
-    public final void setEmail(String email) {
+    public final void setEmail(String email) throws IllegalArgumentException {
+        try {
+            ResultSet set = DATABASE_ADAPTER.executeMysqlQuery("SELECT * FROM " + getTableName() + " WHERE email=?;",
+                    new StringParam(email));
+            if (set.next()) {
+                throw new IllegalArgumentException("Email is already in use!");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new IllegalArgumentException("Error occurred setting the email-address");
+        }
         setString("email", email);
     }
 
