7df8b5846c14bf9bf146fc598894620e7c53fad4
Alexander Linder <alexander-linder@gmx.de>
2019-02-14 16:59:58 +0100

    Add more Mysql tests

---
+++ src/test/java/com/csselect/database/mysql/MysqlOrganiserAdapterTest.java
@@ -0,0 +1,93 @@
+package com.csselect.database.mysql;
+
+import com.csselect.game.BinarySelect;
+import com.csselect.game.NumberOfRoundsTermination;
+import com.csselect.game.gamecreation.patterns.GameOptions;
+import com.csselect.game.gamecreation.patterns.Pattern;
+import com.csselect.inject.Injector;
+import com.csselect.inject.MysqlTestClass;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.sql.SQLException;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+
+public class MysqlOrganiserAdapterTest extends MysqlTestClass {
+
+    private MysqlDatabaseAdapter mysqlDatabaseAdapter;
+    private MysqlOrganiserAdapter organiserAdapter;
+
+    private static final String TEST_EMAIL = "test@test.de";
+    private static final String TEST_EMAIL2 = "test2@test.de";
+    private static final String TEST_HASH = "hash";
+    private static final String TEST_SALT = "salt";
+    private static final String TITLE = "title";
+    private static final String DESCRIPTION = "description";
+    private static final String DATASET = "populationGender";
+    private static final String DATABASE_NAME = "PSE";
+
+    @Override
+    public void setUp() {
+        mysqlDatabaseAdapter = (MysqlDatabaseAdapter) Injector.getInstance().getDatabaseAdapter();
+        try {
+            organiserAdapter = new MysqlOrganiserAdapter(TEST_EMAIL, TEST_HASH, TEST_SALT);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            Assert.fail();
+        }
+    }
+
+    @Override
+    public void reset() {
+        try {
+            mysqlDatabaseAdapter.executeMysqlUpdate("DROP DATABASE CS_SELECT;");
+            mysqlDatabaseAdapter.executeMysqlUpdate("DROP DATABASE PSE;", "PSE");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void getActiveGamesEmptyTest() {
+        Assert.assertTrue(organiserAdapter.getActiveGames().isEmpty());
+    }
+
+    @Test
+    public void getTerminatedGamesEmptyTest() {
+        Assert.assertTrue(organiserAdapter.getTerminatedGames().isEmpty());
+    }
+
+    @Test
+    public void patternTest() {
+        GameOptions gameOptions = getCompleteGameOptions();
+        Pattern pattern = new Pattern(gameOptions, TITLE);
+        Assert.assertTrue(organiserAdapter.getPatterns().isEmpty());
+        organiserAdapter.addPattern(pattern);
+        Assert.assertTrue(organiserAdapter.getPatterns().contains(pattern));
+        Assert.assertEquals(1, organiserAdapter.getPatterns().size());
+        Pattern pattern2 = new Pattern(gameOptions, TITLE);
+        organiserAdapter.addPattern(pattern2);
+        Assert.assertEquals(1, organiserAdapter.getPatterns().size());
+    }
+
+    private Collection<String> getEmails() {
+        Set<String> emails = new HashSet<>();
+        emails.add(TEST_EMAIL);
+        emails.add(TEST_EMAIL2);
+        return emails;
+    }
+
+    private GameOptions getCompleteGameOptions() {
+        GameOptions gameOptions = new GameOptions();
+        gameOptions.setTitle(TITLE);
+        gameOptions.setDescription(DESCRIPTION);
+        gameOptions.setDataset(DATASET);
+        gameOptions.setGamemode(new BinarySelect());
+        gameOptions.setTermination(new NumberOfRoundsTermination(10));
+        gameOptions.setResultDatabaseName(DATABASE_NAME);
+        gameOptions.addInvitedEmails(getEmails());
+        return gameOptions;
+    }
+}
---
+++ src/test/java/com/csselect/database/mysql/MysqlPlayerAdapterTest.java
@@ -0,0 +1,79 @@
+package com.csselect.database.mysql;
+
+import com.csselect.gamification.PlayerStats;
+import com.csselect.inject.Injector;
+import com.csselect.inject.MysqlTestClass;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.sql.SQLException;
+
+public class MysqlPlayerAdapterTest extends MysqlTestClass {
+
+    private MysqlDatabaseAdapter mysqlDatabaseAdapter;
+    private MysqlPlayerAdapter playerAdapter;
+
+    private static final String TEST_EMAIL = "test@test.de";
+    private static final String TEST_EMAIL2 = "test2@test.de";
+    private static final String TEST_HASH = "hash";
+    private static final String TEST_SALT = "salt";
+    private static final String TEST_USERNAME = "tester";
+    private static final String TEST_USERNAME2 = "tester2";
+
+    @Override
+    public void setUp() {
+        mysqlDatabaseAdapter = (MysqlDatabaseAdapter) Injector.getInstance().getDatabaseAdapter();
+        try {
+            playerAdapter = new MysqlPlayerAdapter(TEST_USERNAME, TEST_EMAIL, TEST_HASH, TEST_SALT);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            Assert.fail();
+        }
+    }
+
+    @Override
+    public void reset() {
+        try {
+            mysqlDatabaseAdapter.executeMysqlUpdate("DROP DATABASE CS_SELECT;");
+            mysqlDatabaseAdapter.executeMysqlUpdate("DROP DATABASE PSE;", "PSE");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void getPlayerStatsTest() {
+        PlayerStats p1 = playerAdapter.getPlayerStats();
+        PlayerStats p2 = playerAdapter.getPlayerStats();
+        Assert.assertSame(p1, p2);
+    }
+
+    @Test
+    public void getInvitedGamesEmptyTest() {
+        Assert.assertTrue(playerAdapter.getInvitedGames().isEmpty());
+    }
+
+    @Test
+    public void getRoundsEmptyTest() {
+        Assert.assertTrue(playerAdapter.getRounds().isEmpty());
+    }
+
+    @Test
+    public void getActiveGamesEmptyTest() {
+        Assert.assertTrue(playerAdapter.getActiveGames().isEmpty());
+    }
+
+    @Test
+    public void getTerminatedGamesEmptyTest() {
+        Assert.assertTrue(playerAdapter.getTerminatedGames().isEmpty());
+    }
+
+    @SuppressWarnings("SimplifiableJUnitAssertion") //Assert.assertNotEquals method is suggested but doesn't exist
+    @Test
+    public void equalsTest() throws SQLException {
+        MysqlPlayerAdapter playerAdapter2 = new MysqlPlayerAdapter(TEST_USERNAME2, TEST_EMAIL2, TEST_HASH, TEST_SALT);
+        Assert.assertEquals(playerAdapter, playerAdapter);
+        Assert.assertFalse(playerAdapter.equals(playerAdapter2));
+        Assert.assertFalse(playerAdapter2.equals(playerAdapter));
+    }
+}
