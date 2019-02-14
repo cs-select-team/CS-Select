b3f221d1cb9e41e749e26802c6c391bd3c2d7acb
Alexander Linder <alexander-linder@gmx.de>
2019-02-14 13:26:29 +0100

    Increase coverage in gamecreation

--- src/test/java/com/csselect/game/gamecreation/CreationTests.java
+++ src/test/java/com/csselect/game/gamecreation/CreationTests.java
@@ -1,8 +1,15 @@
 package com.csselect.game.gamecreation;
 
+import com.csselect.game.BinarySelect;
+import com.csselect.game.NumberOfRoundsTermination;
+import com.csselect.game.gamecreation.patterns.Pattern;
 import com.csselect.inject.TestClass;
 import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
 
 public class CreationTests extends TestClass {
     private GameCreator builder;
@@ -33,27 +40,69 @@ public class CreationTests extends TestClass {
     @Test
     public void testSetDescription() {
         builder.setOption(DESCRIPTION_KEY, "TEST");
-        Assert.assertEquals("TEST", builder.getGameOptions().getTitle());
+        Assert.assertEquals("TEST", builder.getGameOptions().getDescription());
+    }
+
+    @Test
+    public void testSetDataset() {
+        builder.setOption(FEATURESET_KEY, "TEST");
+        Assert.assertEquals("TEST", builder.getGameOptions().getDataset());
     }
 
     @Test
     public void testSetTermination() {
-        builder.setOption(TERMINATION_KEY, "rounds:50");
+        builder.setOption(TERMINATION_KEY, NumberOfRoundsTermination.TYPE + ":50");
         Assert.assertNotNull(builder.getGameOptions().getTermination());
+        Assert.assertTrue(builder.getGameOptions().getTermination() instanceof NumberOfRoundsTermination);
+    }
+
+    @Test
+    public void testSetResultDatabaseName() {
+        builder.setOption(DATABASE_KEY, "database");
+        Assert.assertEquals("database", builder.getGameOptions().getResultDatabaseName());
     }
 
     @Test
     public void testSetGamemodeBinary() {
-        builder.setOption(GAMEMODE_KEY, "binary:50");
+        builder.setOption(GAMEMODE_KEY, BinarySelect.TYPE + ":50");
         Assert.assertNotNull(builder.getGameOptions().getGamemode());
+        Assert.assertTrue(builder.getGameOptions().getGamemode() instanceof BinarySelect);
     }
 
     @Test
     public void testAddPlayers() {
-        builder.setOption(ADD_PLAYERS_KEY, "3@yahoo.de:1@gmail.com:2@gmx.de");
+        builder.setOption(ADD_PLAYERS_KEY, "3@yahoo.de,1@gmail.com,2@gmx.de");
         Assert.assertTrue(builder.getGameOptions().getInvitedEmails().contains("3@yahoo.de"));
         Assert.assertTrue(builder.getGameOptions().getInvitedEmails().contains("1@gmail.com"));
         Assert.assertTrue(builder.getGameOptions().getInvitedEmails().contains("2@gmx.de"));
+    }
+
+    @Test
+    public void testMakePattern() {
+        fullySetOptions();
+        Pattern pattern = builder.makePattern("test");
+        Assert.assertNotNull(pattern);
+        Assert.assertEquals("test", pattern.getTitle());
+        builder.loadPattern(pattern);
+        Assert.assertTrue(builder.getGameOptions().isComplete());
+    }
+
+    @Test
+    public void testRemoveEmails() {
+        builder.getGameOptions().addInvitedEmails(getEmails());
+        Assert.assertTrue(builder.getGameOptions().getInvitedEmails().contains(TITLE_KEY));
+        Assert.assertTrue(builder.getGameOptions().getInvitedEmails().contains(DESCRIPTION_KEY));
+        builder.getGameOptions().removeInvitedEmails(getEmails());
+        Assert.assertTrue(builder.getGameOptions().getInvitedEmails().isEmpty());
+    }
+
+    @Test
+    public void testResetEmails() {
+        builder.getGameOptions().addInvitedEmails(getEmails());
+        Assert.assertTrue(builder.getGameOptions().getInvitedEmails().contains(TITLE_KEY));
+        Assert.assertTrue(builder.getGameOptions().getInvitedEmails().contains(DESCRIPTION_KEY));
+        builder.getGameOptions().resetEmails();
+        Assert.assertTrue(builder.getGameOptions().getInvitedEmails().isEmpty());
     }
 
     @Test
@@ -63,12 +112,23 @@ public class CreationTests extends TestClass {
 
     @Test
     public void isComplete() {
+        fullySetOptions();
+        Assert.assertTrue(builder.getGameOptions().isComplete());
+    }
+
+    private void fullySetOptions() {
         builder.setOption(TITLE_KEY, "title");
         builder.setOption(DESCRIPTION_KEY, "description");
         builder.setOption(DATABASE_KEY, "database");
         builder.setOption(FEATURESET_KEY, "populationGender");
-        builder.setOption(GAMEMODE_KEY, "binary:50");
-        builder.setOption(TERMINATION_KEY, "rounds:50");
-        Assert.assertTrue(builder.getGameOptions().isComplete());
+        builder.setOption(GAMEMODE_KEY, BinarySelect.TYPE + ":50");
+        builder.setOption(TERMINATION_KEY, NumberOfRoundsTermination.TYPE + ":50");
+    }
+
+    private Collection<String> getEmails() {
+        Set<String> emails = new HashSet<>();
+        emails.add(TITLE_KEY);
+        emails.add(DESCRIPTION_KEY);
+        return emails;
     }
 }
