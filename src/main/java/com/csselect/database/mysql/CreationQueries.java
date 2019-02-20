3974b5cb417e66a5bf938427de4eace65f8d779d
Alexander Linder <alexander-linder@gmx.de>
2019-02-20 03:27:14 +0100

    Refactor creationQuueries to use table name constants

--- src/main/java/com/csselect/database/mysql/CreationQueries.java
+++ src/main/java/com/csselect/database/mysql/CreationQueries.java
@@ -10,84 +10,87 @@ final class CreationQueries {
     @Language("sql")
     static final String CREATE_PLAYER_TABLE
             = "CREATE TABLE IF NOT EXISTS " + TableNames.PLAYERS + "("
-            + "id INT AUTO_INCREMENT PRIMARY KEY,"
-            + "hash VARCHAR(255),"
-            + "salt VARCHAR(255),"
-            + "email VARCHAR(255),"
-            + "language VARCHAR(255),"
-            + "username VARCHAR(255));";
+            + ColumnNames.ID + " INT AUTO_INCREMENT PRIMARY KEY,"
+            + ColumnNames.HASH + " VARCHAR(255),"
+            + ColumnNames.SALT + " VARCHAR(255),"
+            + ColumnNames.EMAIL + " VARCHAR(255),"
+            + ColumnNames.LANGUAGE + " VARCHAR(255),"
+            + ColumnNames.USERNAME + " VARCHAR(255));";
 
     @Language("sql")
     static final String CREATE_ORGANISER_TABLE
             = "CREATE TABLE IF NOT EXISTS " + TableNames.ORGANISERS + "("
-            + "id INT AUTO_INCREMENT PRIMARY KEY,"
-            + "hash VARCHAR(255),"
-            + "salt VARCHAR(255),"
-            + "email VARCHAR(255),"
-            + "language VARCHAR(255));";
+            + ColumnNames.ID + " INT AUTO_INCREMENT PRIMARY KEY,"
+            + ColumnNames.HASH + " VARCHAR(255),"
+            + ColumnNames.SALT + " VARCHAR(255),"
+            + ColumnNames.EMAIL + " VARCHAR(255),"
+            + ColumnNames.LANGUAGE + " VARCHAR(255));";
 
     @Language("sql")
     static final String CREATE_GAME_TABLE
             = "CREATE TABLE IF NOT EXISTS " + TableNames.GAMES + "("
-            + "id INT AUTO_INCREMENT PRIMARY KEY,"
-            + "organiserId INT,"
-            + "INDEX organiser_ind (organiserId),"
-            + "FOREIGN KEY (organiserId) REFERENCES organisers(id) ON DELETE CASCADE,"
-            + "title VARCHAR(255),"
-            + "description VARCHAR(255),"
-            + "databasename VARCHAR(255),"
-            + "isTerminated BOOLEAN,"
-            + "dataset VARCHAR(255),"
-            + "termination VARCHAR(255),"
-            + "gamemode VARCHAR(255));";
+            + ColumnNames.ID + " INT AUTO_INCREMENT PRIMARY KEY,"
+            + ColumnNames.ORGANISER_ID + " INT,"
+            + "INDEX organiser_ind (" + ColumnNames.ORGANISER_ID + "),"
+            + "FOREIGN KEY (" + ColumnNames.ORGANISER_ID + ") REFERENCES "
+            + TableNames.ORGANISERS + "(" + ColumnNames.ID + ") ON DELETE CASCADE,"
+            + ColumnNames.TITLE + " VARCHAR(255),"
+            + ColumnNames.DESCRIPTION + " VARCHAR(255),"
+            + ColumnNames.DATABASE_NAME + " VARCHAR(255),"
+            + ColumnNames.IS_TERMINATED + " BOOLEAN,"
+            + ColumnNames.DATASET + " VARCHAR(255),"
+            + ColumnNames.TERMINATION + " VARCHAR(255),"
+            + ColumnNames.GAMEMODE + " VARCHAR(255));";
 
     @Language("sql")
     static final String CREATE_PATTERN_TABLE
             = "CREATE TABLE IF NOT EXISTS " + TableNames.PATTERNS + "("
-            + "organiserId INT,"
-            + "INDEX organiser_ind (organiserId),"
-            + "FOREIGN KEY (organiserId) REFERENCES organisers(id) ON DELETE CASCADE,"
-            + "title VARCHAR(255),"
-            + "PRIMARY KEY (organiserId, title),"
-            + "gameTitle VARCHAR(255),"
-            + "description VARCHAR(255),"
-            + "dataset VARCHAR(255),"
-            + "databasename VARCHAR(40),"
-            + "termination VARCHAR(255),"
-            + "gamemode VARCHAR(255),"
-            + "invitedPlayers VARCHAR(255));";
+            + ColumnNames.ORGANISER_ID + " INT,"
+            + "INDEX organiser_ind (" + ColumnNames.ORGANISER_ID + "),"
+            + "FOREIGN KEY (" + ColumnNames.ORGANISER_ID + ") REFERENCES "
+            + TableNames.ORGANISERS + "(" + ColumnNames.ID + ") ON DELETE CASCADE,"
+            + ColumnNames.TITLE + " VARCHAR(255),"
+            + "PRIMARY KEY (" + ColumnNames.ORGANISER_ID + ", " + ColumnNames.TITLE + "),"
+            + ColumnNames.GAME_TITLE + " VARCHAR(255),"
+            + ColumnNames.DESCRIPTION + " VARCHAR(255),"
+            + ColumnNames.DATASET + " VARCHAR(255),"
+            + ColumnNames.DATABASE_NAME + " VARCHAR(40),"
+            + ColumnNames.TERMINATION + " VARCHAR(255),"
+            + ColumnNames.GAMEMODE + " VARCHAR(255),"
+            + ColumnNames.INVITED_PLAYERS + " VARCHAR(255));";
 
     @Language("sql")
     static final String CREATE_PLAYERSTATS_TABLE
             = "CREATE TABLE IF NOT EXISTS " + TableNames.PLAYERSTATS + "("
-            + "id INT PRIMARY KEY ,"
-            + "INDEX player_ind (id),"
-            + "FOREIGN KEY (id) REFERENCES players(id) ON DELETE CASCADE,"
-            + "score INT,"
-            + "roundsPlayed INT,"
-            + "dailiesCompleted INT,"
-            + "maxRoundScore INT,"
-            + "lastScore INT,"
-            + "highestStreak INT);";
+            + ColumnNames.ID + " INT PRIMARY KEY ,"
+            + "INDEX player_ind (" + ColumnNames.ID + "),"
+            + "FOREIGN KEY (" + ColumnNames.ID + ") REFERENCES "
+            + TableNames.PLAYERS + "(" + ColumnNames.ID + ") ON DELETE CASCADE,"
+            + ColumnNames.SCORE + " INT,"
+            + ColumnNames.ROUNDS_PLAYED + " INT,"
+            + ColumnNames.DAILIES_COMPLETED + " INT,"
+            + ColumnNames.MAX_ROUND_SCORE + " INT,"
+            + ColumnNames.LAST_SCORE + " INT,"
+            + ColumnNames.HIGHEST_STREAK + " INT);";
 
     @Language("sql")
     static final String CREATE_ROUNDS_TABLE
             = "CREATE TABLE IF NOT EXISTS " + TableNames.GAME_ROUNDS + "("
-            + "id INT AUTO_INCREMENT PRIMARY KEY,"
-            + "playerId INT,"
-            + "time DATETIME,"
-            + "skipped BOOLEAN,"
-            + "quality DOUBLE,"
-            + "points INT,"
-            + "uselessFeatures VARCHAR(255),"
-            + "chosenFeatures VARCHAR(255),"
-            + "shownFeatures VARCHAR(255));";
+            + ColumnNames.ID + " INT AUTO_INCREMENT PRIMARY KEY,"
+            + ColumnNames.PLAYER_ID + " INT,"
+            + ColumnNames.TIME + " DATETIME,"
+            + ColumnNames.SKIPPED + " BOOLEAN,"
+            + ColumnNames.QUALITY + " DOUBLE,"
+            + ColumnNames.POINTS + " INT,"
+            + ColumnNames.USELESS_FEATURES + " VARCHAR(255),"
+            + ColumnNames.CHOSEN_FEATURES + " VARCHAR(255),"
+            + ColumnNames.SHOWN_FEATURES + " VARCHAR(255));";
 
     @Language("sql")
     static final String CREATE_GAMES_PLAYERS_TABLE
             = "CREATE TABLE IF NOT EXISTS " + TableNames.GAME_PLAYERS + "("
-            + "email VARCHAR(255) PRIMARY KEY,"
-            + "invited BOOLEAN);";
+            + ColumnNames.EMAIL + " VARCHAR(255) PRIMARY KEY,"
+            + ColumnNames.INVITED + " BOOLEAN);";
 
     private CreationQueries() {
         //Utility-classes shouldn't be instantiated
