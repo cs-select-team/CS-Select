0ceefa3cfe0cad868d74178b3855df85a97805ce
Alexander Linder <alexander-linder@gmx.de>
2019-03-01 17:11:00 +0100

    Cleanup servlet::getBody

--- src/main/java/com/csselect/API/httpAPI/Servlet.java
+++ src/main/java/com/csselect/API/httpAPI/Servlet.java
@@ -204,7 +204,6 @@ public abstract  class Servlet extends HttpServlet {
      */
     static String getBody(HttpServletRequest request) throws IOException {
 
-        String body = null;
         StringBuilder stringBuilder = new StringBuilder();
         BufferedReader bufferedReader = null;
 
@@ -213,27 +212,18 @@ public abstract  class Servlet extends HttpServlet {
             if (inputStream != null) {
                 bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
                 char[] charBuffer = new char[128];
-                int bytesRead = -1;
+                int bytesRead;
                 while ((bytesRead = bufferedReader.read(charBuffer)) > 0) {
                     stringBuilder.append(charBuffer, 0, bytesRead);
                 }
-            } else {
-                stringBuilder.append("");
             }
-        } catch (IOException ex) {
-            throw ex;
         } finally {
             if (bufferedReader != null) {
-                try {
-                    bufferedReader.close();
-                } catch (IOException ex) {
-                    throw ex;
-                }
+                bufferedReader.close();
             }
         }
 
-        body = stringBuilder.toString();
-        return body;
+        return stringBuilder.toString();
     }
 
     /** method to overwrite to handle a GET request that this object receives
