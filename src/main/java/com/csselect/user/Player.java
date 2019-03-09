9b4cb37345b3cc7c3b0a8468a45197f215af20ba
Julian <julian.dinh@web.de>
2019-03-09 19:18:11 +0100

    Fix typos in JavaDoc and localisation

--- src/main/java/com/csselect/user/Player.java
+++ src/main/java/com/csselect/user/Player.java
@@ -136,7 +136,7 @@ public class Player extends User {
 
     /**
      * To retrieve the player's stats, it is necessary to get access to the according {@link Gamification} interface.
-     * @return {@link PlayerStats} object belonging to the player
+     * @return {@link Gamification} object belonging to the player
      */
     public Gamification getStats() {
         return playerAdapter.getPlayerStats();
